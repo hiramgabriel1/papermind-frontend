84c6ba7dd4167421832de736f97995151aa1b2e7
hiramgabriel-dev <hiram.gabriel@atratopago.com>
2025-01-22 10:03:03 -0700

    eslint error solved

--- src/app/Inicio/layout.tsx
+++ src/app/Inicio/layout.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import React, { useState } from "react";
@@ -7,68 +6,60 @@ import UserComponent from "@/components/user/page";
 import NavComponent from "@/components/nav/page";
 import AddChatComponent from "@/components/buttons/addchat/page";
 import HeaderComponent from "@/components/buttons/header/page";
-import logo from "../../assets/logo.png";
-import perfil from "../../assets/perfil.png";
-import { FaPlus } from "react-icons/fa";
-import { FiMessageCircle } from "react-icons/fi";
-import { FaRegLightbulb } from "react-icons/fa";
-import { TbMovie } from "react-icons/tb";
-import { MdOutlinePhotoSizeSelectActual } from "react-icons/md";
-import { PiCat } from "react-icons/pi";
-import { IoEarthOutline } from "react-icons/io5";
-import { BiSearchAlt } from "react-icons/bi";
-import { GoKebabHorizontal } from "react-icons/go";
-import Image from "next/image";
 
-export const metadata = {
-	title: "Papermind - Inicio",
-	description: "Ask everything you want!",
-};
-
+/**
+ *
+ * this page is the layout of the page, it contains the sidebar and the content
+ *
+ * @param children
+ * @returns
+ */
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
+	const toggleSidebar = () => {
+		setIsSidebarOpen(!isSidebarOpen);
+	};
 
-  return (
-    <main className="h-screen flex overflow-hidden relative">
-      <div className="lg:hidden p-4 bg-background_secundary z-20">
-        <button onClick={toggleSidebar} className="text-black">
-          <GiHamburgerMenu className="w-6 h-6" />
-        </button>
-      </div>
+	return (
+		<main className="h-screen flex overflow-hidden relative">
+			<div className="lg:hidden p-4 bg-background_secundary z-20">
+				<button onClick={toggleSidebar} className="text-black">
+					<GiHamburgerMenu className="w-6 h-6" />
+				</button>
+			</div>
 
-      {isSidebarOpen && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-50 z-10 lg:hidden"
-          onClick={toggleSidebar}
-        />
-      )}
+			{isSidebarOpen && (
+				<div
+					className="fixed inset-0 bg-black bg-opacity-50 z-10 lg:hidden"
+					onClick={toggleSidebar}
+				/>
+			)}
 
-      <aside
-        className={`bg-background_primary p-6 flex flex-col justify-between transition-transform duration-300 ease-in-out lg:block fixed lg:static z-20 h-full lg:h-auto w-3/4 sm:w-1/2 lg:w-auto ${
-          isSidebarOpen ? "translate-x-0" : "-translate-x-full lg:translate-x-0"
-        }`}
-      >
-        <div className="flex flex-col h-full">
-          <HeaderComponent />
+			<aside
+				className={`bg-background_primary p-6 flex flex-col justify-between transition-transform duration-300 ease-in-out lg:block fixed lg:static z-20 h-full lg:h-auto w-3/4 sm:w-1/2 lg:w-auto ${
+					isSidebarOpen ? "translate-x-0" : "-translate-x-full lg:translate-x-0"
+				}`}
+			>
+				<div className="flex flex-col h-full">
+					<HeaderComponent />
 
-          <div className="flex-grow flex flex-col mt-5 justify-between">
-            <AddChatComponent />
-            <NavComponent />
-          </div>
+					<div className="flex-grow flex flex-col mt-5 justify-between">
+						<AddChatComponent />
+						<NavComponent />
+					</div>
 
-          <UserComponent />
-        </div>
-      </aside>
+					<UserComponent />
+				</div>
+			</aside>
 
-      <main className="flex-1 p-2 pr-8 bg-background_secundary overflow-auto">{children}</main>
-    </main>
-  );
+			<main className="flex-1 p-2 pr-8 bg-background_secundary overflow-auto">
+				{children}
+			</main>
+		</main>
+	);
 }
